4d0661c357365d74ac90fe5fc6344adf46e03d56
Vatsal Uppal <vatsal.uppal@gmail.com>
2020-06-13 11:27:23 +0530

    Added service function to sign up a user

--- app/mod_auth/service.py
+++ app/mod_auth/service.py
@@ -1,5 +1,6 @@
+from app import db
 from .models import Employee
-from .exceptions import UserNotFound, InvalidPassword
+from .exceptions import UserNotFound, InvalidPassword, UserAlreadyExists, PasswordDoNotMatch
 
 
 def login_user(username, password):
@@ -26,3 +27,37 @@ def login_user(username, password):
         raise InvalidPassword
 
     return employee.employee_id, employee.username
+
+
+def signup_user(username, password, confirm_password):
+    '''
+    Verifies username, password and confirm_password
+    and creates a new employee account
+
+    Parameters:
+        username: string or False
+        password: string or False
+        confirm_password: string or False
+
+    returns:
+        employee_id: string
+        username: string
+    '''
+
+    if username == False or password == False or confirm_password == False:
+        raise ValueError('One of the required field is missing')
+    if password != confirm_password:
+        raise PasswordDoNotMatch()
+
+    employee_exists = Employee.query.filter_by(username=username).first()
+
+    if employee_exists is not None:
+        raise UserAlreadyExists(username)
+
+    new_employee = Employee(username, password)
+    db.session.add(new_employee)
+    db.session.commit()
+    db.session.flush()
+
+    return new_employee.employee_id, new_employee.username
+    
