4ba783f9363c01ff820437ab16b8bf567088f6c8
Vatsal Uppal <vatsal.uppal@gmail.com>
2020-06-13 12:33:28 +0530

    [CHANGED]: Added field customer id - changed type of ssn id to String of length 10 - used format in repr - made function to generate random_nine_digit_id

--- app/mod_customer/models.py
+++ app/mod_customer/models.py
@@ -1,38 +1,41 @@
-import random
-import string
-from random import randint
-
+import random, string
 from app import db
 
 
-def generate_customer_id():
-    '''
-    Generates a 9 digit customer id
-    '''
-    range_start = 10**(10-1)
-    range_end = 10**10-1
-    return randint(range_start,range_end)
+def random_nine_digit_id():
+    charset = string.digits
+    return ''.join(
+        [
+            random.choice(charset) for _ in range(9)
+        ]
+    )
 
 
 class Customer(db.Model):
     __tablename__ = 'customer'
 
+    customer_id = db.Column(db.String(20), primary_key=True)
 
     customer_ssn_id = db.Column(
-        db.BigInteger(),  nullable=False, primary_key=True
+        db.String(10),  nullable=False, unique=True
     )
+
     customer_name = db.Column(
         db.String(30), nullable=False
     )
+
     customer_age = db.Column(
-        db.BigInteger(), nullable=False
+        db.Integer(), nullable=False
     )
-    customer_address =  db.Column(
+
+    customer_address = db.Column(
         db.String(500), nullable=False
     )
+
     customer_state = db.Column(
         db.String(30), nullable=False
     )
+
     customer_city = db.Column(
         db.String(30), nullable=False
     )
@@ -45,9 +48,5 @@ class Customer(db.Model):
         self.customer_state = customer_state
         self.customer_city = customer_city
 
-
-  #  def __repr__(self):
-   #     return '<%d %d %s %d %s %s %s>'%(customer_id = self.customer_id, customer_ssn_id = self.customer_ssn_id, customer_name = self.customer_name, customer_age = self.customer_age, customer_address = self.customer_address, customer_state = self.customer_state, customer_city = self.customer_city )
-
     def __repr__(self):
-        return f"Customer('{self.customer_ssn_id}','{self.customer_name}','{self.customer_age}','{self.customer_address}','{self.customer_state}','{self.customer_city}')"
+        return "Customer('{cust_ssn}','{cust_name}','{cust_age}','{cust_addr}','{cust_state}','{cust_city}')".format(cust_ssn=self.customer_ssn_id, cust_name=self.customer_name, cust_age=self.customer_age, cust_addr=self.customer_address, cust_state=self.customer_state, cust_city=self.customer_city)
