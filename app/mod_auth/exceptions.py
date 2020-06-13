bcc316d4d32776e49de15d122c005940de7f21dc
Vatsal Uppal <vatsal.uppal@gmail.com>
2020-06-13 11:26:08 +0530

    Added new custome exceptions for employee signup

--- app/mod_auth/exceptions.py
+++ app/mod_auth/exceptions.py
@@ -11,10 +11,35 @@ class UserNotFound(Exception):
         self.message = message
         super().__init__(self.message)
 
+
 class InvalidPassword(Exception):
     '''
     Exception raised when the password is invalid
     '''
-    
+
     def __init__(self):
         super().__init__('Invalid username or password')
+
+
+class UserAlreadyExists(Exception):
+    '''
+    Exception raised when trying to sign up and a user with the same username already exists
+
+    Attributes:
+        username: username that already exists
+    '''
+
+    def __init__(self, username):
+        self.message = 'A user with the username {username} already exists'.format(
+            username=username)
+        super().__init__(self.message)
+
+
+class PasswordDoNotMatch(Exception):
+    '''
+    Exception raised when password and confirm password do not match
+    '''
+
+    def __init__(self):
+        self.message = 'Password and confirm password do not match'
+        super().__init__(self.message)
