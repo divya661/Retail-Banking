159d00c36b4072e40cca4f8d5e22115c5e887e90
Vatsal Uppal <vatsal.uppal@gmail.com>
2020-06-13 11:26:33 +0530

    Added route for signup

--- app/mod_auth/routes.py
+++ app/mod_auth/routes.py
@@ -1,14 +1,20 @@
 from flask import Blueprint, render_template, request, flash, session, url_for, redirect
-from .service import login_user
-from .exceptions import UserNotFound, InvalidPassword
+from .service import login_user, signup_user
+from .exceptions import UserNotFound, InvalidPassword, UserAlreadyExists, PasswordDoNotMatch
 
 bp_auth = Blueprint(
     'auth', __name__, template_folder='templates', static_folder='static'
 )
 
+def already_logged_in():
+    if 'employee_id' in session and 'username' in session:
+        return True
+    return False
 
 @bp_auth.route('/', methods=['GET', 'POST'])
 def login():
+    if already_logged_in():
+        return redirect(url_for('site_root'))
     if request.method == 'POST':
         username = request.form.get('username', False)
         password = request.form.get('password', False)
@@ -21,5 +27,28 @@ def login():
             flash('Invalid username or password', 'error')
         except InvalidPassword as invalid_password:
             flash('Invalid username or password', 'error')
+        except ValueError as value_error:
+            flash(value_error.message, 'error')
     return render_template('login.html')
+
+@bp_auth.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if already_logged_in():
+        return redirect(url_for('site_root'))
+    if request.method == 'POST':
+        username = request.form.get('username', False)
+        password = request.form.get('password', False)
+        confirm_password = request.form.get('confirmPassword', False)
+        try:
+            employee_id, username = signup_user(username, password, confirm_password)
+            session['employee_id'] = employee_id
+            session['username'] = username
+            return redirect(url_for('site_root'))
+        except UserAlreadyExists as user_already_exists:
+            flash(user_already_exists.message, 'error')
+        except PasswordDoNotMatch as password_do_not_match:
+            flash(password_do_not_match.message, 'error')
+        except ValueError as value_error:
+            flash(value_error.message, 'error')
+    return render_template('signup.html')
 
