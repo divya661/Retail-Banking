ce871ea9421cad3f9a6a32932e27543a16adf0ce
Vatsal Uppal <vatsal.uppal@gmail.com>
2020-06-15 15:25:04 +0530

    Added customer does not exist exception

--- app/mod_customer/exceptions.py
+++ app/mod_customer/exceptions.py
@@ -6,3 +6,15 @@ class InvalidSSNId(Exception):
     def __init__(self):
         self.message = 'Invalid SSN ID. Either in use or less than 9 digits'
         super().__init__(self.message)
+
+
+class CustomerDoesNotExist(Exception):
+    '''
+    Exception raised when customer with provided ssn id and id does not exist
+    '''
+
+    def __init__(self, customer_ssn_id, customer_id):
+        self.customer_ssn_id = customer_ssn_id
+        self.customer_id = customer_id
+        self.message = 'customer with SSN ID {ssn} and ID {id} does not exist'.format(ssn=self.customer_ssn_id, id=self.customer_id)
+        super().__init__(self.message)
